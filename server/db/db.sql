f8fffa350397331b9b3e296e6c3eb284c567e9d8
Cotldus <78590013+cotldus@users.noreply.github.com>
2021-08-06 15:48:38 +0800

    Update db.sql to include create restaurants

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,3 +1,20 @@
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50),
+    price_range INT check(price_range >=1 and price_range <= 5)
+);
+
+INSERT INTO restaurants (name, location, price_range) values ('mcdonalds', 'new york' , 1);
+INSERT INTO restaurants (name, location, price_range) values ('taco bell', 'Lincoln' , 2);
+INSERT INTO restaurants (name, location, price_range) values ('taco bell', 'dallas' , 2);
+INSERT INTO restaurants (name, location, price_range) values ('taco bell', 'houston' , 2);
+INSERT INTO restaurants (name, location, price_range) values ('cheesecake factory', 'Cali' , 3);
+INSERT INTO restaurants (name, location, price_range) values ('cheesecake factory', 'new york' ,4);
+INSERT INTO restaurants (name, location, price_range) values ('cheesecake factory', 'Lincoln' , 3);
+INSERT INTO restaurants (name, location, price_range) values ('wendys', 'new york' , 2);
+INSERT INTO restaurants (name, location, price_range) values ('pizza kitchen', 'new york' , 3);
+
 CREATE TABLE reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
